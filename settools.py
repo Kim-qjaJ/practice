389678a25e87a7df3edf6e9ee52244d7cedf0ba7
Kim BJ <100742437+Kim-qjaJ@users.noreply.github.com>
2022-05-04 11:37:24 +0900

    Update settools.py

--- settools.py
+++ settools.py
@@ -8,7 +8,7 @@ setuptools.setup (
   author='Kim-qjaJ',
   author_email='neom9245@gmail.com',
   url="https://github.com/Kim-qjaJ/practice"
-  download_url="https://github.com/Kim-qjaJ/practice/archive/refs/tags/v0.4.0.zip",
+  download_url = "https://github.com/Kim-qjaJ/practice/archive/refs/tags/v0.4.0.zip",
   install_requires=['pyt'],
   long_description = 'oss practice py module',
   long_description_content_type = 'text/markdown',
