200746f4c9823738a7a94d113bc74ce07b795e3d
Kim BJ <100742437+Kim-qjaJ@users.noreply.github.com>
2022-05-04 11:32:07 +0900

    Update settools.py

--- settools.py
+++ settools.py
@@ -1,6 +1,6 @@
 import setuptools
 
-setuptools.setup(
+setuptools.setup (
   include_package_data = True,
   name='uhm',
   version='0.4.0',
@@ -10,8 +10,8 @@ setuptools.setup(
   url="https://github.com/Kim-qjaJ/practice"
   download_url="https://github.com/Kim-qjaJ/practice/archive/refs/tags/v0.4.0.zip",
   install_requires=['pyt'],
-  long_description='oss practice py module',
-  long_description_content_type='text/markdown',
+  long_description = 'oss practice py module',
+  long_description_content_type = 'text/markdown',
   classifiers=[
     "Programming Language :: Python :: 3",
     "Operating System :: OS Independent:",
